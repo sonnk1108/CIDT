d1ef26d54342dccd4c1fc3abb09f1cdf340a73f5
nkson1108 <tranlientrang1998@gmail.com>
2020-07-07 15:06:44 +0700

    Added my project

---
+++ eclipse-workspace/Mail2/src/Main/MailConfig.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class MailConfig {
+	public static final String HOST_NAME = "smtp.gmail.com";
+	 
+    public static final int SSL_PORT = 465; // Port for SSL
+ 
+    public static final int TSL_PORT = 587; // Port for TLS/STARTTLS
+ 
+    public static final String APP_EMAIL = "gpcodervn@gmail.com"; // your email
+ 
+    public static final String APP_PASSWORD = "ihzgdgiy3udgemdjhhw"; // your password
+ 
+    public static final String RECEIVE_EMAIL = "gpcodervn@gmail.com"; 
+
+
+}
